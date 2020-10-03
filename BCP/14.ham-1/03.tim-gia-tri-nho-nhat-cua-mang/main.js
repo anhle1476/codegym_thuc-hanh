1196998a045964fa6753ffa6eb9e58aafeec63c9
anhle1476 <anhle1476@gmail.com>
2020-10-03 15:58:47 +0700

    done BCP_14_03

---
+++ BCP/14.ham-1/03.tim-gia-tri-nho-nhat-cua-mang/main.js
@@ -0,0 +1,16 @@
+function minArray(arr) {
+  if (arr.length == 0) return -1;
+
+  let min = arr[0];
+  let index = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      index = i;
+    }
+  }
+
+  console.log(`Min: ${min} - Index: ${index}`);
+  return index;
+}
