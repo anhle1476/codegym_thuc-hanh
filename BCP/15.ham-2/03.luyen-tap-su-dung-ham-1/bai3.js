04369b27afe05323880ebbe3f9c16fa5467db8b1
anhle1476 <anhle1476@gmail.com>
2020-10-03 17:19:22 +0700

    done BCP_15_03

---
+++ BCP/15.ham-2/03.luyen-tap-su-dung-ham-1/bai3.js
@@ -0,0 +1,7 @@
+const process = (num1, num2) => {
+  if (num1 > num2) {
+    alert(num1 + " > " + num2);
+  } else {
+    return num1 + num2;
+  }
+};
---
+++ BCP/15.ham-2/03.luyen-tap-su-dung-ham-1/bai4.js
@@ -0,0 +1,12 @@
+let result = 0;
+
+function addNumbers() {
+  alert(result);
+  firstNum = 4;
+  secondNum = 8;
+  result = firstNum + secondNum;
+  alert(result);
+  return result;
+}
+
+result = addNumbers();
