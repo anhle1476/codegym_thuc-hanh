9548e72955df984f3c9e0a80f6dea2fcba81d1db
anhle1476 <anhle1476@gmail.com>
2020-11-16 16:50:04 +0700

    done apj-04-05

---
+++ APJ/04.lop-va-doi-tuong/05.access-modifier/src/mypack/B.java
@@ -0,0 +1,46 @@
+//package mypack;
+//
+//import pack.*;
+//
+//class B{
+//
+//    public static void main(String args[]){
+//
+////        A obj = new A();//Compile Time Error
+//
+////        obj.msg();//Compile Time Error
+//
+//    }
+//
+//}
+
+package mypack;
+
+import pack.*;
+
+
+
+//class B extends A{
+//
+//    public static void main(String args[]){
+//
+//        B obj = new B();
+//
+//        obj.msg();
+//
+//    }
+//
+//}
+
+class B{
+
+    public static void main(String args[]){
+
+        A obj = new A();
+
+        obj.msg();
+
+    }
+
+}
+
