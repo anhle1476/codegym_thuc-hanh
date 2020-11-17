bed63388813bce74bee8e1623ae76a63c18771e9
anhle1476 <anhle1476@gmail.com>
2020-11-17 22:54:42 +0700

    done apj-06-02

---
+++ APJ/06.ke-thua/02.lop-circle-va-lop-cylinder/src/CylinderTest.java
@@ -0,0 +1,7 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(5, "red", 10);
+        System.out.println(cylinder);
+        System.out.println(cylinder.getVolume());
+    }
+}
