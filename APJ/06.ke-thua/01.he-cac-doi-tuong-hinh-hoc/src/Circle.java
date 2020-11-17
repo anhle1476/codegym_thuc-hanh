8536ad69cabda201da9d68d1232e5c0766687fe5
anhle1476 <anhle1476@gmail.com>
2020-11-17 22:42:19 +0700

    done apj-06-01

---
+++ APJ/06.ke-thua/01.he-cac-doi-tuong-hinh-hoc/src/Circle.java
@@ -0,0 +1,39 @@
+import javax.crypto.Cipher;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle() {
+        this(1);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return radius * 2 * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius=" + radius + ", which is a subclass of " + super.toString();
+    }
+}
---
+++ APJ/06.ke-thua/01.he-cac-doi-tuong-hinh-hoc/src/Rectangle.java
@@ -0,0 +1,47 @@
+public class Rectangle extends Shape {
+    double width, height;
+
+    public Rectangle() {
+        this(1.0, 1.0);
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "A Rectangle with width=" + width + " and length=" + height + ", which is a subclass of " + super.toString();
+    }
+}
