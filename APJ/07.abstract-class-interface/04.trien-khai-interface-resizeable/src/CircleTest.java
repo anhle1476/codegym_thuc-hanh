cffd81518faafdac6df496a5fa68f3d56531f16c
anhle1476 <anhle1476@gmail.com>
2020-11-19 14:09:03 +0700

    done apj-07-04

---
+++ APJ/07.abstract-class-interface/04.trien-khai-interface-resizeable/src/CircleTest.java
@@ -0,0 +1,8 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(1);
+        System.out.println(circle);
+        circle.resize();
+        System.out.println(circle);
+    }
+}
---
+++ APJ/07.abstract-class-interface/04.trien-khai-interface-resizeable/src/RectangleTest.java
@@ -0,0 +1,8 @@
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2, 1);
+        System.out.println(rectangle);
+        rectangle.resize();
+        System.out.println(rectangle);
+    }
+}
