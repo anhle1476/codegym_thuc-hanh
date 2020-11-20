88314e91e96c8836c90c4c48dbafbcc42bed7186
anhle1476 <anhle1476@gmail.com>
2020-11-20 08:14:37 +0700

    done apj-09-02

---
+++ APJ/09.automated-testing-tdd/02.absolute-number-calculator/test/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    @DisplayName("Testing 0")
+    void testFindAbsolute0() {
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Testing 1")
+    void testFindAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(result, expected);
+    }
+
+    @Test
+    @DisplayName("Testing -1")
+    void testFindAbsoluteNeg1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(result, expected);
+    }
+}
