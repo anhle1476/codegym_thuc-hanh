badaf692f9e618bbd3f2f12b2b3eb659f1f6c29a
anhle1476 <anhle1476@gmail.com>
2020-11-28 16:33:32 +0700

    modify insertion-sort logic

--- APJ/14.thuat-toan-sap-xep/03.minh-hoa-sap-xep-chen/src/Main.java
+++ APJ/14.thuat-toan-sap-xep/03.minh-hoa-sap-xep-chen/src/Main.java
@@ -11,18 +11,21 @@ public class Main {
     public static void insertionSort(int[] array) {
         int completeBlock = 0;
         for (int elementIndex = completeBlock + 1; elementIndex < array.length; elementIndex++) {
-            for (int completedIndex = 0; completedIndex <= completeBlock; completedIndex++) {
-                if (array[elementIndex] < array[completedIndex]) {
-                    insertElement(array, elementIndex, completedIndex);
+            for (int completedIndex = completeBlock; completedIndex >= 0; completedIndex--) {
+                if (array[elementIndex] > array[completedIndex]) {
+                    insertElement(array, elementIndex, completedIndex + 1);
                     break;
                 }
             }
+            if (array[elementIndex] < array[0])
+                insertElement(array, elementIndex, 0);
             System.out.println("After " + (completeBlock + 1) + " loops: " + Arrays.toString(array));
             completeBlock++;
         }
     }
 
     public static void insertElement(int[] array, int fromIndex, int insertIndex) {
+        if (fromIndex == insertIndex) return;
         System.out.println("Insert from index " + fromIndex + " to index " + insertIndex);
         int holder = array[fromIndex];
         if (fromIndex - insertIndex >= 0)
--- APJ/14.thuat-toan-sap-xep/04.cai-dat-thuat-toan-sap-xep-chen/src/Main.java
+++ APJ/14.thuat-toan-sap-xep/04.cai-dat-thuat-toan-sap-xep-chen/src/Main.java
@@ -10,17 +10,20 @@ public class Main {
     public static void insertionSort(int[] array) {
         int completeBlock = 0;
         for (int elementIndex = completeBlock + 1; elementIndex < array.length; elementIndex++) {
-            for (int completedIndex = 0; completedIndex <= completeBlock; completedIndex++) {
-                if (array[elementIndex] < array[completedIndex]) {
-                    insertElement(array, elementIndex, completedIndex);
+            for (int completedIndex = completeBlock; completedIndex >= 0; completedIndex--) {
+                if (array[elementIndex] > array[completedIndex]) {
+                    insertElement(array, elementIndex, completedIndex + 1);
                     break;
                 }
             }
+            if (array[elementIndex] < array[0])
+                insertElement(array, elementIndex, 0);
             completeBlock++;
         }
     }
 
     public static void insertElement(int[] array, int fromIndex, int insertIndex) {
+        if (fromIndex == insertIndex) return;
         int holder = array[fromIndex];
         if (fromIndex - insertIndex >= 0)
             System.arraycopy(array, insertIndex, array, insertIndex + 1, fromIndex - insertIndex);
